5ab6d2056b8951581cb1e91d94af505e4751b2a5
michaelrogers <contact@mhrogers.com>
2016-10-06 09:22:53 -0500

    Refactored the functions to meet ES6 syntax; revised onload to DOMContentLoaded event listener
    Reordered function calls to be consistent with load order. Revised a few
    variables to make better use of hoisting.

--- adder-circuit.js
+++ adder-circuit.js
@@ -1,87 +1,82 @@
-window.onload = init;
+document.addEventListener('DOMContentLoaded', () => {
+	
+	const AND_Gate = (A, B) => {return A && B;}
 
-function init(){
-	clickListener();
-	// $('#switchRowA label div').each(function(){$(this).attr("id",)});
-}
+	const OR_Gate = (A, B) => {return A || B;}
 
-function clickListener(){
-	// $('.switchRow').on('click', 'label div', getSwitchValues);
-	$('#projectBox').on('click', getSwitchValues);
-}
+	const NAND_Gate = (A, B) => {return !(A && B);}
 
-function getSwitchValues(){
-	let A = {},
-		B = {};
-	let A_ID, B_ID;
-	for (let v=0; v<8; v++){
-		A_ID = 'A'+ v; B_ID = 'B'+ v;
-		A[v] = document.getElementById(A_ID).checked;
-		B[v] = document.getElementById(B_ID).checked;
+	const XOR_Gate = (A, B) => { //XOR constructed using OR gate, NAND gate, and AND gate
+		let orResult = OR_Gate(A,B);
+		let nandResult = NAND_Gate (A,B);
+		return AND_Gate(orResult,nandResult);
 	}
-	addingMachine(A,B);
-}
 
-function illuminateIndicators(results, finalCarryIn){
-	let ind_ID; 
-	for (let x=0; x<8; x++){
-		ind_ID = 'ind'+ x;
-		if (results[x]){
-			document.getElementById(ind_ID).className = "indicator lightOn";
+	const halfAdder = (A, B) => {
+		//halfAdder constructed using XOR gate and AND gate
+		let sumOut = XOR_Gate(A,B);
+		let carryOut = AND_Gate(A,B);
+		return {sumOut: sumOut, carryOut: carryOut};
+	}
+
+	const fullAdder = (carryIn, A, B) => {
+		//fullAdder constructed using two halfAdders;
+		//the input for the first is A,B; 
+		//input for the second is carryIn and sumOut of first half adder
+		let firstHalfAdder = halfAdder(A,B);
+		let secondHalfAdder = halfAdder(carryIn, firstHalfAdder.sumOut);
+		let carryOut = OR_Gate(firstHalfAdder.carryOut,secondHalfAdder.carryOut);
+		// console.table({A, B, firstHalfAdder, secondHalfAdder, carryOut});
+		return {
+			sumOut: secondHalfAdder.sumOut,
+			carryOut: carryOut
+		};
+	}
+
+	const addingMachine = (A, B) => {
+		let carryIn = false; //The initial carryIn = false or 0 bit
+		let results = [];
+		let eachFullAdder;
+		for (let x = 0; x < A.length; x++){
+			eachFullAdder = fullAdder(carryIn, A[x], B[x]);
+			results[x] = eachFullAdder.sumOut;
+			carryIn = eachFullAdder.carryOut;
 		}
-		else {
-			document.getElementById(ind_ID).className = "indicator";
+		illuminateIndicators(results, carryIn);
+	}
+
+	const illuminateIndicators = (results, finalCarryIn) => {
+		let eachIndicator;
+		let ind8 = document.getElementById('ind8');
+		for (let x=0; x<8; x++){
+			eachIndicator = document.getElementById('ind'+ x);
+			results[x] ? eachIndicator.className = "indicator lightOn" : eachIndicator.className = "indicator"; 
+					}
+		finalCarryIn ? ind8.className = "indicator lightOn" : ind8.className = "indicator";
+		
+	}
+
+	const getSwitchValues = () => {
+		let A = [];
+		let B = [];
+		const switches = [].slice.call(document.querySelectorAll('#switchRowA input'));
+		for (let v = 0; v < switches.length; v++){
+			A[v] = document.getElementById('A'+ v).checked;
+			B[v] = document.getElementById('B'+ v).checked;
 		}
+		addingMachine(A, B);
 	}
-	if (finalCarryIn){
-		document.getElementById('ind8').className = "indicator lightOn";
+	const clickListener = () => {
+		document.getElementById('projectBox').addEventListener('change', getSwitchValues);
 	}
-	else {
-		document.getElementById('ind8').className = "indicator";
+
+	const init = () => {
+		clickListener();
+		// $('#switchRowA label div').each(function(){$(this).attr("id",)});
 	}
-}
 
-function addingMachine(A,B){
-	let carryIn = false; //The initial carryIn = false or 0 bit
-	let results = {};
-	let eachFullAdder;
-	for (let x=0; x < 8; x++){
-		eachFullAdder = fullAdder(carryIn,A[x],B[x]);
-		results[x] = eachFullAdder.sumOut;
-		carryIn = eachFullAdder.carryOut;
-	}
-	// console.log(results);
-	illuminateIndicators(results, carryIn);
-}
+	init();
 
-function fullAdder(carryIn,A,B){//fullAdder constructed using two halfAdders; the input for the first is A,B; input for the second is carryIn and sumOut of first half adder
-	let firstHalfAdder = halfAdder(A,B);
-	let secondHalfAdder = halfAdder(carryIn, firstHalfAdder.sumOut);
-	let carryOut = OR_Gate(firstHalfAdder.carryOut,secondHalfAdder.carryOut);
-	// console.table({A: A, B: B, firstHalfAdder: firstHalfAdder, secondHalfAdder: secondHalfAdder, carryOut: carryOut});
-	return {sumOut: secondHalfAdder.sumOut, carryOut: carryOut};
-}
+});
+ // DOMcontentloaded
 
-function halfAdder(A, B){//halfAdder constructed using XOR gate and AND gate
-	let sumOut = XOR_Gate(A,B);
-	let carryOut = AND_Gate(A,B);
-	return {sumOut: sumOut, carryOut: carryOut};
-}
-
-function XOR_Gate(A,B){ //XOR constructed using OR gate, NAND gate, and AND gate
-	let orResult = OR_Gate(A,B);
-	let nandResult = NAND_Gate (A,B);
-	return AND_Gate(orResult,nandResult);
-}
-
-function AND_Gate(A,B){
-	return A && B;
-}
-
-function OR_Gate(A,B){
-	return A || B;
-}
-
-function NAND_Gate (A,B){
-	return !(A && B);
-}
